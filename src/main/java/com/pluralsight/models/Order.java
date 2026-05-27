f81c2b32c0f183055c310ff32d7507abfada2e9c
Mamii868 <marcmotenii@gmail.com>
2026-05-27 10:12:59 -0500

    Add order class

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,28 @@
 package com.pluralsight.models;
 
+import java.util.List;
+
 public class Order {
+    private String deliveryMethod;
+    private List<MenuItem> items;
+    private double orderTotal;
+
+    public Order() {
+    }
+
+    public void setDeliveryMethod(String deliveryMethod) {
+        this.deliveryMethod = deliveryMethod;
+    }
+
+    public String getDeliveryMethod() {
+        return deliveryMethod;
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+
+    public double getOrderTotal() {
+        return orderTotal;
+    }
 }
