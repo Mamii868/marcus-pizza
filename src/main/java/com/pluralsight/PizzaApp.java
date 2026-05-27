6e57fda8760cbacb409aac8e1ae18917ea4c6026
Mamii868 <marcmotenii@gmail.com>
2026-05-27 13:22:27 -0500

    Add run code

--- src/main/java/com/pluralsight/PizzaApp.java
+++ src/main/java/com/pluralsight/PizzaApp.java
@@ -1,16 +1,13 @@
 package com.pluralsight;
 
 import com.pluralsight.models.Crust;
+import com.pluralsight.models.Order;
 import com.pluralsight.models.Pizza;
 import com.pluralsight.models.Topping;
+import com.pluralsight.ui.Menu;
 
 public class PizzaApp {
     public static void main(String[] args) {
-
-
-        Pizza pizza = new Pizza();
-
-        pizza.setCrust(Crust.DEEP_DISH);
-        pizza.addTopping(Topping.PEPPERONI);
+        Menu.mainMenu();
     }
 }
