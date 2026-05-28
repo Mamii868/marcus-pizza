de577ce6fe450b71545052873fd47c7a5e5fc999
Mamii868 <marcmotenii@gmail.com>
2026-05-28 18:21:25 -0500

    Fix receipt path bug

--- server/src/main/java/com/pluralsight/receipt/ReceiptWriter.java
+++ server/src/main/java/com/pluralsight/receipt/ReceiptWriter.java
@@ -17,7 +17,7 @@ public class ReceiptWriter {
     public static String createReceipt(Order order) {
         String timeStamp = generateTimeStamp();
         try {
-            FileWriter fileWriter = new FileWriter("src/main/resources/receipts/" + timeStamp + ".txt");
+            FileWriter fileWriter = new FileWriter("server/src/main/resources/receipts/" + timeStamp + ".txt");
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             StringBuilder receiptToReturn = new StringBuilder();
 
