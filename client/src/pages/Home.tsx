6dacd6f0244af3f28f3fcfec7a96ef0d05c8d294
Mamii868 <marcmotenii@gmail.com>
2026-05-28 18:31:17 -0500

    Install tailwind and delete boilerplate

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to Marcus Pizza!</h1>
+            <p>Order your favorite pizza now!</p>
+        </div>
+    );
+}
+export default Home;
