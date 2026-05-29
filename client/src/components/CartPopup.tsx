2412d455d3ae9bc82de02726d4d55d98f14b07d1
Mamii868 <marcmotenii@gmail.com>
2026-05-29 00:26:44 -0500

    Add checkout footer

--- client/src/components/CartPopup.tsx
+++ client/src/components/CartPopup.tsx
@@ -1,18 +1,20 @@
 import { useCartContext } from "../providers/CartProvider";
 import type { Pizza } from "../types/pizzaTypes";
+import Button from "./Button";
 
 const CartPopup = () => {
   const { cart, cartAmount, cartError } = useCartContext();
   return (
     <div className="cart w-1/2 h-full absolute top-0 right-0 bg-dark-bg p-4 rounded-l-2xl border border-border text-white flex flex-col gap-4">
       <h2 className="text-2xl font-bold mb-4 text-center">Your Cart</h2>
-      <div className="cartItems flex flex-col gap-2">
+      <div className="cartItems flex flex-col gap-2 overflow-y-scroll">
         {cartAmount === 0 ? (
           <p className="text-lg font-bold">Your cart is empty</p>
         ) : (
           cart.items.map((item, index) => {
             if ("crust" in item) {
               // Weird type conversion to get around pizzas being within the cart of menu items
+              //   Yes this can break but I trust in the server
               const pizza = item as unknown as Pizza;
               return (
                 <div key={index} className="border border-border p-2 rounded-lg">
@@ -37,6 +39,10 @@ const CartPopup = () => {
           })
         )}
         {cartError && <p className="text-red-500 text-lg font-bold mt-2 text-center">{cartError}</p>}
+      </div>
+      <div className="footer border-t-2 py-2 border-border flex items-center justify-between px-4">
+        <p className="text-xl font-bold">Total: ${cart.items.reduce((total, item) => total + (item.price || 0), 0).toFixed(2)}</p>
+        <Button>Checkout</Button>
       </div>
     </div>
   );
