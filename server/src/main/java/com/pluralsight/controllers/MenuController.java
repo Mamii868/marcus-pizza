b496aebc941b9e28e6887b4a8e33abf2d83388da
Mamii868 <marcmotenii@gmail.com>
2026-05-28 19:44:44 -0500

    Add all other menus

--- server/src/main/java/com/pluralsight/controllers/MenuController.java
+++ server/src/main/java/com/pluralsight/controllers/MenuController.java
@@ -1,13 +1,38 @@
 package com.pluralsight.controllers;
 
-import com.pluralsight.models.SpecialtyPizza;
+import com.pluralsight.models.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MenuController {
 
-    @GetMapping("/pizza/specialties")
+    @GetMapping("/menu/sizes")
+    Size[] getSizes() {
+        return Size.values();
+    }
+
+    @GetMapping("/menu/crusts")
+    Crust[] getCrusts() {
+        return Crust.values();
+    }
+
+    @GetMapping("/menu/toppings")
+    Topping[] getToppings() {
+        return Topping.values();
+    }
+
+    @GetMapping("/menu/sides")
+    Side[] getSides() {
+        return Side.values();
+    }
+
+    @GetMapping("/menu/drinks")
+    Drink[] getDrinks() {
+        return Drink.values();
+    }
+
+    @GetMapping("/menu/specialties")
     SpecialtyPizza[] getSpecialties() {
         return SpecialtyPizza.values();
     }
