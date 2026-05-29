775aab7925c5a50161a9be9e9d17fcd000efb648
Mamii868 <marcmotenii@gmail.com>
2026-05-28 20:14:20 -0500

    Add links to other menus

--- client/src/pages/Order.tsx
+++ client/src/pages/Order.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getSpecialties } from "../services/menuService";
 import Card from "../components/Card";
 import type { Specialty } from "../types/pizzaTypes";
+import { Link } from "react-router";
 
 const Order = () => {
   const [specialties, setSpecialties] = useState<Specialty[]>();
@@ -16,14 +17,30 @@ const Order = () => {
   }, []);
 
   return (
-    <div className="w-full">
+    <div className="w-full text-white my-10">
       <div className="featured">
-        <h2>Featured</h2>
-        <div className="featuredList w-full flex flex-col md:flex-row gap-4">
+        <h2 className="text-2xl font-bold mb-4 text-center">Featured</h2>
+        <div className="featuredList w-full flex flex-col md:flex-row gap-4 p-4">
           {specialties &&
-            specialties.map((specialty) => (
-              <Card key={specialty.name} title={specialty.name} toppings={specialty.toppings} crust={"Regular"} price={specialty.price} />
-            ))}
+            specialties
+              .slice(0, 3)
+              .map((specialty) => (
+                <Card key={specialty.name} title={specialty.name} toppings={specialty.toppings} crust={"Regular"} price={specialty.price} />
+              ))}
+        </div>
+      </div>
+      <div className="menu">
+        <h2 className="text-2xl font-bold mb-4 text-center">Menu</h2>
+        <div className="menuList w-full flex flex-col gap-4 p-4">
+          <Link to="pizzas" className="w-full bg-dark-bg p-4 rounded-2xl border border-border text-left">
+            <p className="text-lg font-bold">Pizzas</p>
+          </Link>
+          <Link to="sides" className="w-full bg-dark-bg p-4 rounded-2xl border border-border text-left">
+            <p className="text-lg font-bold">Sides</p>
+          </Link>
+          <Link to="drinks" className="w-full bg-dark-bg p-4 rounded-2xl border border-border text-left">
+            <p className="text-lg font-bold">Drinks</p>
+          </Link>
         </div>
       </div>
     </div>
