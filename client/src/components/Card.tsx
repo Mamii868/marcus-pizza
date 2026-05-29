5e48bf2552c950e72a58fb2f362cc943ed897e11
Mamii868 <marcmotenii@gmail.com>
2026-05-28 20:08:10 -0500

    Add better layouts

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,4 +1,5 @@
 import type { Topping } from "../types/pizzaTypes";
+import Button from "./Button";
 
 interface CardProps {
   title: string;
@@ -9,17 +10,18 @@ interface CardProps {
 
 const Card = ({ title, toppings, crust, price }: CardProps) => {
   return (
-    <div className="card w-full flex flex-col gap-2 items-center justify-center p-2">
+    <div className="card w-full flex flex-col items-center gap-2 text-center p-2 text-white bg-dark-bg rounded-2xl border border-border">
       <div className="title">
-        <h2>{title}</h2>
+        <h2 className="text-xl font-bold border-b-2 border-orange">{title}</h2>
       </div>
       <p>{crust} Crust</p>
-      <div className="toppingList">
-        {toppings.map((topping) => (
-          <p key={topping.name}>- {topping.name}</p>
+      <div className="toppingList flex flex-col items-center grow">
+        {toppings.map((topping, index) => (
+          <p className="text-sm text-left w-full" key={index}>- {topping.name}</p>
         ))}
       </div>
-      <p>${price.toFixed(2)}</p>
+      <p className="text-lg font-bold">${price.toFixed(2)}</p>
+      <Button text="Add to Cart" />
     </div>
   );
 };
