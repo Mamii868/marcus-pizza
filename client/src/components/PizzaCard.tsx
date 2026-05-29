cb55988f66b00e4d0fadebb90e2e03fa37c9703a
Mamii868 <marcmotenii@gmail.com>
2026-05-29 01:37:06 -0500

    Add size select

--- client/src/components/PizzaCard.tsx
+++ client/src/components/PizzaCard.tsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import { useCartContext } from "../providers/CartProvider";
-import type { Pizza, Specialty } from "../types/pizzaTypes";
+import type { Pizza, Size, Specialty } from "../types/pizzaTypes";
 import Button from "./Button";
+import { getSizes } from "../services/menuService";
 
 const PizzaCard = ({ name, toppings, price }: Specialty) => {
   const { addPizzaToCart } = useCartContext();
+  const [size, setSize] = useState<string>("SMALL");
+  const [sizeOptions, setSizeOptions] = useState<Size[]>([]);
+  useEffect(() => {
+    const fetchSize = async () => {
+      // This is really just to get the price of the size since the pizza price is based on a medium pizza
+      const data = await getSizes();
+      setSizeOptions(data);
+    };
+    fetchSize();
+  }, []);
+
   const handleAddToCart = () => {
     const pizza: Pizza = {
       crust: { name: "REGULAR" },
-      size: { name: "MEDIUM" },
+      size: { name: size },
       toppings: toppings.map((topping) => ({ name: topping.name.toUpperCase() })),
     };
 
@@ -26,7 +39,19 @@ const PizzaCard = ({ name, toppings, price }: Specialty) => {
           </p>
         ))}
       </div>
-      <p className="text-lg font-bold">${price.toFixed(2)}</p>
+      <div className="sizeSelect flex gap-2">
+        {sizeOptions.map((option) => (
+          <button
+            key={option.name}
+            className={`px-2 py-1 rounded ${size === option.name.toUpperCase().split(" ")[0].replace("-", "_") ? "bg-orange border border-orange text-white" : "bg-dark-lightbg text-white border border-orange cursor-pointer hover:bg-darkorange"} transition duration-200 text-sm`}
+            onClick={() => setSize(option.name.toUpperCase().split(" ")[0].replace("-", "_"))}>
+            {option.name}
+          </button>
+        ))}
+      </div>
+      <p className="text-lg font-bold">
+        ${(price + (sizeOptions.find((option) => option.name.toUpperCase().split(" ")[0].replace("-", "_") === size)?.price || 0)).toFixed(2)}
+      </p>
       <Button onClick={handleAddToCart}>Add to Cart</Button>
     </div>
   );
